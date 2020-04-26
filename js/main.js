b2d6861a906d6f8a0bd27fa173d86ef438a8045c
Denys Petrushkov <denis.p@cheitgroup.com>
2020-04-26 21:10:54 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 jQuery(document).ready(function($) {
     
-   
+  $('.modal-close a').click(function() {
+    $('.modal-overlay').hide();
+  });
+
+  $('.modal-overlay').click(function(e) {
+    var clicked = $(e.target);
+    if (clicked.is('.modal') || clicked.parents().is('.modal')) {
+    } else {
+      $('.modal-overlay').hide();
+    }
+  });
 
 });
 
